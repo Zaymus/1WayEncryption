55c7bfb29d7b30b8f0828bc4b178092ffc9028bb
Carson <carsonbrown47@gmail.com>
2019-02-12 18:57:45 -0500

    created encryption algorithm
    prints in reverse order and does not shift over 1 unit to the left

--- Challenges/wayEncryption.java
+++ Challenges/wayEncryption.java
@@ -3,36 +3,38 @@ import java.util.*;
 public class wayEncryption
 {
 	
-	public static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args)
+	static Scanner sc = new Scanner(System.in);
+	static int newVal1 = 0, newVal2 = 0, encryptedVal;
+		public static void main(String[] args)
 	{
 		while(true) {
 			System.out.println("input a number");
-			
 			int num = sc.nextInt();
-			
+			System.out.print("\nEncrypted number: ");
 			encrypt(num);
 		}
 	}//end of main
-	
-	/*
-		a function which takes an integer greater than 99999,
+
+	/*		a function which takes an integer greater than 99999,
 		
-		- adds 4 to each digit,
-		- shifts digits 1 to the left
-		- If the digit is greater than 9 subtract 10
+				- adds 4 to each digit,
+				- shifts digits 1 to the left
+				- If the digit is greater than 9 subtract 10
 		
-		no arrays, strings or chars
-	*/
+			no arrays, strings or chars		*/
 	public static void encrypt(int val)
-	{
-		int num = val;
-		int length = 0;
-		while(num > 0)
+	{		
+		while(val != 0)
 		{
-			num /= 10;
-			length++;
-        }//end of while
-        System.out.println("length: " + length);
+			newVal1 = (val % 10) + 4;
+			if (newVal1 > 9)
+			{
+				newVal1 -= 10;
+			}//end of if
+			val /= 10;
+			
+			System.out.print(newVal1);
+		}//end of while	
+		System.out.println("\n");
 	}//end of encrypt
 }//end of class
