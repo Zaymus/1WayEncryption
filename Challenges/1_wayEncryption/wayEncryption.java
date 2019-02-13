0a60b310a25e274eeede5f9ee50bade79e782f4f
Carson Brown <45776517+Zaymus@users.noreply.github.com>
2019-02-13 14:19:08 -0500

    added getLength function

--- Challenges/1_wayEncryption/wayEncryption.java
+++ Challenges/1_wayEncryption/wayEncryption.java
@@ -22,19 +22,30 @@ public class wayEncryption
 				- If the digit is greater than 9 subtract 10
 		
 			no arrays, strings or chars		*/
-	public static void encrypt(int val)
+	public static void encrypt(int num)
 	{		
-		while(val != 0)
+		while(num != 0)
 		{
 			newVal1 = (val % 10) + 4;
 			if (newVal1 > 9)
 			{
 				newVal1 -= 10;
 			}//end of if
-			val /= 10;
+			num /= 10;
 			
 			System.out.print(newVal1);
 		}//end of while	
 		System.out.println("\n");
 	}//end of encrypt
+	
+	public static int getLength(int num)
+	{
+		int l = 0;
+		while(num != 0)
+		{
+			num /= 10;
+			l++;
+		}//end of while
+		return l;
+	}//end of getLength
 }//end of class
